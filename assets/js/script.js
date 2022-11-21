2a3d5ccfb3a938800a5b372bf6fc4dc423d16afd
john33mcd <jmcd-34@hotmail.com>
2022-11-21 13:52:26 +0000

    added javascript which allows player to cycle through questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,90 @@
-const buttonTest = document.getElementById("test")
-buttonTest.addEventListener("click", runGame)
-const questionElement = document.getElementById("question")
-const answerButtonsElement = document.getElementById("answer-buttons")
+const buttonStart = document.getElementById("start")
+buttonStart.addEventListener("click", runGame)
+const questionElement = document.getElementById('question')
+const answerButtonElement = document.getElementById('answerBtn')
 
 /**
  * this function begins when the start game button is pushed, calls questions from the array of questions, 
  * assigns the questions to the relevant button, also makes the start quiz button disappear.
  */
+ let shuffledQuestions, currentQuestionIndex
+
 function runGame() {
-    showQuestion()
-    document.getElementById("question").innerHTML = questions[0].question;
-    document.getElementById("answerOne").innerHTML = questions[0].answers[0].text;
-    document.getElementById("answerTwo").innerHTML = questions[0].answers[1].text;
-    document.getElementById("answerThree").innerHTML = questions[0].answers[2].text;
-    document.getElementById("answerFour").innerHTML = questions[0].answers[3].text;
-    document.getElementById("test").style.display = 'none';
+
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    setNextQuestion()
+    /* document.getElementById("start").style.display = 'none' */
 }
 
-function checkAnswer() {
-
-}
-
-function selectAnswer() {
-
-}
-
-function getCorrectAnswer() {
-
-}
 
 function setNextQuestion() {
-}
-
-function showQuestion(){
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 
 }
+
+function showQuestion(question) {
+    document.getElementById("question").innerHTML = questions[0].question;
+    document.getElementById("answerOne").innerHTML = questions[0].answers[0].text
+    document.getElementById("answerTwo").innerHTML = questions[0].answers[1].text
+    document.getElementById("answerThree").innerHTML = questions[0].answers[2].text
+    document.getElementById("answerFour").innerHTML = questions[0].answers[3].text
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerBtnElement.appendChild(button)
+    }
+
+
+function selectAnswer (){
+
+}
+
+
+
 
 /**
  * this will create the arrays of questions to be added to the HTML 
  */
 var questions = [{
-    question: "Who won the 2022 Tour De France?",
-    answers: [{
-            text: 'Jonas Vindegaard',
-            correct: true
-        },
-        {
-            text: 'Tadej Pogacar',
-            correct: false
-        },
-        {
-            text: 'Geirant Thomas',
-            correct: false
-        },
-        {
-            text: 'Wout Van Aert',
-            correct: false
-        }
-    ]
-}]
+        question: "Who won the 2022 Tour De France?",
+        answers: [{
+                text: 'Jonas Vindegaard',
+                correct: true
+            },
+            {
+                text: 'Tadej Pogacar',
+                correct: false
+            },
+            {
+                text: 'Geirant Thomas',
+                correct: false
+            },
+            {
+                text: 'Wout Van Aert',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: "In what Capital City will the 2022 Tour De France start ?",
+        answers: [{
+                text: 'Paris',
+                correct: false
+            },
+            {
+                text: 'Copenhagan',
+                correct: true
+            },
+            {
+                text: 'Dublin',
+                correct: false
+            },
+            {
+                text: 'London',
+                correct: false
+            }
+        ]
+    }
+]
